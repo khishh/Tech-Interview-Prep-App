fefbf191294f2e684c843d422cc7b57f1908ed2f
khishh <khiraishi0319@gmail.com>
2022-01-08 15:46:27 -0800

    added endpoint based on env

--- client/src/Socket.ts
+++ client/src/Socket.ts
@@ -2,10 +2,12 @@ import { io, Socket, Manager } from "socket.io-client";
 import {ServerToClientEvents, ClientToServerEvents} from '../../model'
 
 // please note that the types are reversed
-let socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:8080');
+
+const endpoint = process.env.NODE_ENV === 'production' ? 'https://tech-interview-prep-app.herokuapp.com/' : 'http://localhost:8080';
+let socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(endpoint);
 
 export const resetSocket = () => {
-    socket = io('http://localhost:8080');
+    socket = io(endpoint);
 }
 
 export {socket};
