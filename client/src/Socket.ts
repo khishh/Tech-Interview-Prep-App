e538f5f4bf475083a5af216454537942c16c8c98
khishh <khiraishi0319@gmail.com>
2022-01-08 14:23:42 -0800

    heroku initial deploy

--- client/src/Socket.ts
+++ client/src/Socket.ts
@@ -1,12 +1,11 @@
 import { io, Socket, Manager } from "socket.io-client";
-import {ServerToClientEvents, ClientToServerEvents} from '../../server/model'
+import {ServerToClientEvents, ClientToServerEvents} from '../../model'
 
 // please note that the types are reversed
-const manager = new Manager('http://localhost:3001');
-let socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:3001');
+let socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:8080');
 
 export const resetSocket = () => {
-    socket = io('http://localhost:3001');
+    socket = io('http://localhost:8080');
 }
 
 export {socket};
